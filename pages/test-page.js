9e6174bdbe3af94d02e481ac6817fcc493abceae
Tuncay Bilgi <tuncay.bilgi39@gmail.com>
2022-10-02 20:05:31 +0200

    added static space.html

---
+++ pages/test-page.js
@@ -0,0 +1,10 @@
+import { PostCard, Categories, PostWidget } from '../components'
+import Head from 'next/head'
+import {getPosts} from '../services'
+import { isYieldExpression } from 'typescript'
+
+
+export default function Home( { posts }) {
+  return (
+    <h1 className='text-white'>yes</h1>    )
+}
