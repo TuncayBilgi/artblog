8ae27399e560fc3169863359e104853cdca56dda
Tuncay Bilgi <tuncay.bilgi39@gmail.com>
2022-10-10 20:37:02 +0200

    Error fix + Categories started
    useEffect error was because of syntax

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React,{useState,useEffect} from 'react'
+import { getCategories } from '../services';
+import Link from 'next/link'
 const Categories = () => {
+  //yes
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {getCategories().then((result)=> setCategories(result))},[])
   return (
-    <div>Categories
-        
+    <div>
+    {categories.map((category) => (
+      <p>test</p>
+    ))}
     </div>
   )
 }
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -3,45 +3,40 @@ import moment from 'moment'
 import Link from 'next/link'
 import { getRecentPosts } from '../services'
 import { getSimilarPosts } from '../services'
-import { getPosts } from '../services'
 
 
 const PostWidget = ({categories,slug}) => {
   const [relatedPosts, setRelatedPosts] = useState([]);
   useEffect(() => {
+    if (slug) { getSimilarPosts().then((result) => setRelatedPosts(result))}
       getRecentPosts()
             .then((result) => setRelatedPosts(result))
-            .then(() => console.log(relatedPosts));
-      //getPosts().then((result) => setRelatedPosts(result))
-    },[]
-  ) 
- 
+  
+},[]) 
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
         {slug ? 'Related Posts' : 'Recent Posts'}
       </h3>
-      {relatedPosts.map((post) => {
+      
+      {relatedPosts.map((post) => (
         <div key={post.title} className='flex items-center w-full mb-4'>
+          
           <div className='w-16 flex-none'>
             <img 
             alt = {post.title}
             height = "60px"
             widht = "60px"
-            className='align-middle rounded'
+            className='align-middle rounded-lg '
             src={post.featureImage.url}
             />
           </div>
-        
         <div className='flex-grow ml-4'>
-          <p className='text-grey-500 font-xs'>{moment(post.createdAt).format('DD MMM, YYYY')}</p>
-          <Link href={`/post/${post.slug}`} key={post.title} className='text-md'>{post.title}</Link>
-        
+          <p className='text-grey-500 text-xs'>{moment(post.createdAt).format('DD MMM, YYYY')}</p>
+          <Link href={`/post/${post.slug}`} key={post.title} className='text-md font-semibold'>{post.title}</Link>
         </div>
-
         </div>
-      })}
-      PostWidget
+      ))}
         
     </div>
   )
